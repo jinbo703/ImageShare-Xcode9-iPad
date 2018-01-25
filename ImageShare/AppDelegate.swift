97c0edd6a36d566b9ea6f2cdab9d60c41ae3d759
John Nik <appdev@soundsoftware.ca>
2018-01-25 21:29:00 +0800

    update

--- ImageShare/AppDelegate.swift
+++ ImageShare/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  ImageShare
 //
-//  Created by PAC on 11/8/17.
-//  Copyright © 2017 PAC. All rights reserved.
+//  Created by John Nik on 11/8/17.
+//  Copyright © 2017 johnik703. All rights reserved.
 //
 
 import UIKit
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         return true
     }
 
--- ImageShare/ViewController.swift
+++ ImageShare/ViewController.swift
@@ -2,24 +2,108 @@
 //  ViewController.swift
 //  ImageShare
 //
-//  Created by PAC on 11/8/17.
-//  Copyright © 2017 PAC. All rights reserved.
+//  Created by John Nik on 11/8/17.
+//  Copyright © 2017 johnik703. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDropInteractionDelegate, UIDragInteractionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        navigationItem.title = "Collage Sharing"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
+        
+        view.addInteraction(UIDropInteraction(delegate: self))
+        view.addInteraction(UIDragInteraction(delegate: self))
+    }
+    
+    @objc func handleShare() {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return }
+        UIGraphicsEndImageContext()
+        
+        let activityViewController = UIActivityViewController(activityItems: [image, "this is awesom"], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+        
+        let touchedPoint = session.location(in: self.view)
+        if let touchedImageView = self.view.hitTest(touchedPoint, with: nil) as? UIImageView {
+            let touchedImage = touchedImageView.image
+            
+            let itemProvider = NSItemProvider(object: touchedImage!)
+            let dragItem = UIDragItem(itemProvider: itemProvider)
+            dragItem.localObject = touchedImageView
+            return [dragItem]
+        }
+        
+        return []
+    }
+    
+    func dragInteraction(_ interaction: UIDragInteraction, previewForLifting item: UIDragItem, session: UIDragSession) -> UITargetedDragPreview? {
+        return UITargetedDragPreview(view: item.localObject as! UIView)
+    }
+    
+    func dragInteraction(_ interaction: UIDragInteraction, willAnimateLiftWith animator: UIDragAnimating, session: UIDragSession) {
+        session.items.forEach { (dragItem) in
+            
+            if let touchedImageView = dragItem.localObject as? UIView {
+                touchedImageView.removeFromSuperview()
+            }
+            
+        }
+    }
+    
+    func dragInteraction(_ interaction: UIDragInteraction, item: UIDragItem, willAnimateCancelWith animator: UIDragAnimating) {
+        
+        self.view.addSubview(item.localObject as! UIView)
+        
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        for dragItem in session.items {
+            dragItem.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (obj, err) in
+                
+                if let err = err {
+                    print("failed to load dragged item", err)
+                    return
+                }
+                guard let draggedImage = obj as? UIImage else { return }
+                
+                DispatchQueue.main.async {
+                    let imageView = UIImageView(image: draggedImage)
+                    imageView.isUserInteractionEnabled = true
+                    imageView.layer.borderWidth = 4
+                    imageView.layer.borderColor = UIColor.black.cgColor
+                    imageView.layer.shadowRadius = 5
+                    imageView.layer.shadowOpacity = 0.3
+                    self.view.addSubview(imageView)
+                    imageView.frame = CGRect(x: 0, y: 0, width: draggedImage.size.width, height: draggedImage.size.height)
+                    
+                    let centerPoint = session.location(in: self.view)
+                    imageView.center = centerPoint
+                }
+                
+            })
+        }
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: UIImage.self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
     }
-
 
 }
 
